c9204b01e06d1563e032537960386701018242be
Yaroslav0102030405 <you@example.com>
2025-05-13 20:59:05 +0300

    types props

---
+++ src/assets/components/Painting/Painting.tsx
@@ -0,0 +1,26 @@
+import type { IPaintingProps } from './type';
+
+import React from 'react';
+
+interface IPaintingProps2 {
+  painting: IPaintingProps[];
+}
+
+const Painting: React.FC<IPaintingProps2> = ({ painting }) => {
+  return (
+    <>
+      <ul>
+        {painting.map(({ name, username }) => (
+          <li key={name}>
+            {/* <img src={url} alt={title} />
+            <h1>{title}</h1> */}
+            <h1>{name}</h1>
+            <p>{username}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Painting;
---
+++ src/assets/components/Painting/type.tsx
@@ -0,0 +1,5 @@
+export interface IPaintingProps {
+  name: string;
+  username: string;
+  email: string;
+}
